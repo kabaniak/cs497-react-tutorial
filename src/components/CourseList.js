393afb83afd1ee031679cbbe8b272c62c90fc26e
kabaniak <kabaniak@gmail.com>
2022-09-28 22:45:56 -0500

    pull json

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -3,11 +3,13 @@ import Course from "./Course";
 const CourseList = ({ courses }) => {
     const termCourses = Object.values(courses)
 
+    console.log(termCourses)
+
     return (
         <div className="course-list">
             {
                 termCourses.map(course =>
-                    <Course course={course} />
+                    <Course key={ course.id } course={course} />
                 )
             }
         </div>
