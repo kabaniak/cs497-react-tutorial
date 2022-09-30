9ceba0695c991f36d623c48c368fc3b25faa1e04
kabaniak <kabaniak@gmail.com>
2022-09-29 19:10:12 -0500

    term buttons

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,19 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import Course from "./Course";
 
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+
 const CourseList = ({ courses }) => {
-    const termCourses = Object.values(courses)
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === course.term);
 
     console.log(termCourses)
 
     return (
-        <div className="course-list">
-            {
-                termCourses.map(course =>
-                    <Course key={ course.id } course={course} />
-                )
-            }
+        <>
+            <TermSelector term={term} setTerm={setTerm} />
+            <div className="course-list">
+                {
+                    termCourses.map(course =>
+                        <Course key={course.term + " " + course.number} course={course} />
+                    )
+                }
+            </div>
+        </>
+    );
+};
+
+
+const TermSelector = ({ term, setTerm }) => {
+    return (
+        <div className="btn-toolbar justify-content-between">
+            <div className="btn-group" id="term-btns">
+                {
+                    Object.values(terms).map(
+                        value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+                    )
+                }
+            </div>
         </div>
     );
 };
+
+const TermButton = ({ term, setTerm, checked }) => (
+    <>
+        <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+            onChange={() => setTerm(term)} />
+        <label className="btn btn-success m-1 p-2" htmlFor={term}>
+            {term}
+        </label>
+    </>
+);
 
 export default CourseList;
